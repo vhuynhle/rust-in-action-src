29a8ef15811104a15fea6b5fe539b4c0857da53d
Van Huynh Le <v.h.le@protonmail.com>
2023-09-05 20:04:55 +0200

    Explicit lifetime notation for functions

---
+++ ch02/src/bin/add-with-lifetime.rs
@@ -0,0 +1,10 @@
+fn add_with_lifetime<'a, 'b>(i: &'a i32, j: &'b i32) -> i32 {
+    i + j
+}
+
+fn main() {
+    let a = 100;
+    let b = 23;
+
+    println!("a + b = {}", add_with_lifetime(&a, &b));
+}
