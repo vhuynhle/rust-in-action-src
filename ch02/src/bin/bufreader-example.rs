4f816d37bb083650d3af6d3d39ac18f5731543ae
Van Huynh Le <v.h.le@protonmail.com>
2023-09-06 09:51:01 +0200

    grep-lite: Allow searching in file and stdin

--- ch02/src/bin/bufreader-example.rs
+++ ch02/src/bin/bufreader-example.rs
@@ -3,10 +3,7 @@ use std::io::BufRead;
 use std::io::BufReader;
 
 fn main() {
-    let filename = std::env::args()
-        .skip(1)
-        .next()
-        .expect("File name not provided");
+    let filename = std::env::args().nth(1).expect("File name not provided");
     let f = File::open(filename).expect("Cannot open file");
     let reader = BufReader::new(f);
 
--- ch02/src/bin/grep-lite.rs
+++ ch02/src/bin/grep-lite.rs
@@ -1,23 +1,37 @@
 use clap::{arg, command};
 use regex::Regex;
+use std::fs::File;
+use std::io;
+use std::io::{BufRead, BufReader};
+
+fn process_lines<T: BufRead>(reader: T, re: Regex) {
+    for (i, line) in reader.lines().enumerate() {
+        let line: String = line.expect("File read error");
+
+        if re.find(&line).is_some() {
+            println!("{}: {}", i, line);
+        }
+    }
+}
 
 fn main() {
     let matches = command!()
         .arg(arg!([pattern] "The pattern to search for").required(true))
+        .arg(arg!([file] "File to search").required(false))
         .get_matches();
 
     let pattern = matches.get_one::<String>("pattern").unwrap();
     let re = Regex::new(pattern).unwrap();
 
-    let quote = "\
-Every face, every shop, bedroom window, public-house, and
-dark square is a picture feverishly turned--in search of what?
-It is the same with books.
- What do we seek through millions of pages?";
+    let input_file = matches.get_one::<String>("file");
 
-    for (i, line) in quote.lines().enumerate() {
-        if re.find(line).is_some() {
-            println!("{}: {}", i, line);
-        }
+    if let Some(input_file) = input_file {
+        let f = File::open(input_file).expect("Cannot open file.");
+        let reader = BufReader::new(f);
+        process_lines(reader, re);
+    } else {
+        let stdin = io::stdin();
+        let reader = stdin.lock();
+        process_lines(reader, re);
     }
 }
