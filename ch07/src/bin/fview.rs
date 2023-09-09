f4d2fcb68cf8a1e25794c27dc0a343800e63eec2
Van Huynh Le <v.h.le@protonmail.com>
2023-09-09 20:45:28 +0200

    Hexview v0.1: With input file

--- ch07/src/bin/fview.rs
+++ ch07/src/bin/fview.rs
@@ -1,18 +1,28 @@
+use std::{
+    fs::File,
+    io::{BufReader, Read},
+};
+
 const BYTE_PER_LINE: usize = 16;
-const INPUT: &[u8] = br#"
-HELLO, WORLD
-"#;
 
 fn main() -> std::io::Result<()> {
-    let buffer: Vec<u8> = INPUT.into();
+    let filename = std::env::args().nth(1).expect("Usage: fview FILENAME");
+    let file = File::open(filename).expect("Cannot open file");
+    let mut bufreader = BufReader::new(file);
+
+    let mut buffer: [u8; BYTE_PER_LINE] = [0; BYTE_PER_LINE];
 
     let mut position_in_input = 0;
-    for line in buffer.chunks(BYTE_PER_LINE) {
+    while let Ok(n) = bufreader.read(&mut buffer) {
         print!("[0x{:08x}] ", position_in_input);
-        for byte in line {
+        for byte in &buffer[0..n] {
             print!("{:02x} ", byte);
         }
         println!();
+        if n < BYTE_PER_LINE {
+            break;
+        }
+
         position_in_input += BYTE_PER_LINE;
     }
 
