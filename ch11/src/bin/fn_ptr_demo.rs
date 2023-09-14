7a8bafba7beefa5ead1a985912476f9cdfc20d05
Van Huynh Le <v.h.le@protonmail.com>
2023-09-14 10:38:45 +0200

    Function pointers

---
+++ ch11/src/bin/fn_ptr_demo.rs
@@ -0,0 +1,6 @@
+fn noop() {}
+
+fn main() {
+    let fn_ptr = noop as usize;
+    println!("noop as usize: 0x{:x}", fn_ptr);
+}
---
+++ ch11/src/bin/fn_ptr_demo_2.rs
@@ -0,0 +1,8 @@
+fn noop() {}
+
+fn main() {
+    let fn_ptr = noop as usize;
+    let typed_fn_ptr = noop as *const fn() -> ();
+    println!("noop as usize:             0x{:x}", fn_ptr);
+    println!("noop as *const fn() -> (): {:p}", typed_fn_ptr);
+}
